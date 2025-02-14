1e326880c53f5614ebeb7a0a4ea98e6e39353f90
LE2D22yanoura <k023g0120@g.neec.ac.jp>
2025-02-14 16:17:42 +0900

    04_01完了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <Novice.h>
 #include "GameManager.h"
 
-const char kWindowTitle[] = "LE2*_**_***_***";
+const char kWindowTitle[] = "LE2D_20_ヤノウラ_ハルト";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
